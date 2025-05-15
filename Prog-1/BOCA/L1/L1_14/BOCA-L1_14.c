3b8bac450a078f811ec00c780ef7e0bbfd38cffe
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-05-14 21:12:37 -0300

    ✨ feat: update L1_14 exercise

--- Prog-1/BOCA/L1/L1_14/BOCA-L1_14.c
+++ Prog-1/BOCA/L1/L1_14/BOCA-L1_14.c
@@ -6,12 +6,13 @@ int main() {
     //entrada
     scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &p1, &p2);
 
-    //saida
+    //contas
     int xmin = (x1 < x2) ? x1 : x2;
     int xmax = (x1 > x2) ? x1 : x2;
     int ymin = (y1 < y2) ? y1 : y2;
     int ymax = (y1 > y2) ? y1 : y2;
 
+    //saida
     if (p1 >= xmin && p1 <= xmax && p2 >= ymin && p2 <= ymax) {
         printf("Dentro\n");
     } else {
