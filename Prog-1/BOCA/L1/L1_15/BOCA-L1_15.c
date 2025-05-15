6993da9c29b7d97a94e47d1ab52e960164de2d01
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-05-14 21:09:56 -0300

    ✨ feat: added L1_15 exercise

--- Prog-1/BOCA/L1/L1_15/BOCA-L1_15.c
+++ Prog-1/BOCA/L1/L1_15/BOCA-L1_15.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int numero, ordem;
+    int numero, ordem, unidade;
 
     //entrada
     scanf("%d %d", &numero, &ordem);
 
-    //saida
+    //contas
+    if ( ordem == 1 ) {
+        unidade = numero % 10;
+    }
+    else if ( ordem == 2 ) {
+        unidade = ( numero / 10 ) % 10;
+    }
+    else if ( ordem == 3 ) {
+        unidade = ( numero / 100 ) % 10;
+    }
 
+    //saida
+    if ( unidade % 2 == 0 ) {
+        printf("PAR");
+    }
+    else {
+        printf("IMPAR");
+    }
 
     return 0;
 }
