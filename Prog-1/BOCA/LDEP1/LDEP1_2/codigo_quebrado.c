4bb7e4eb12661c3078bb6dd616315ccc80a35c7e
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-05-26 15:44:32 -0300

    ✨ feat: add exercise LDEP1_2

---
+++ Prog-1/BOCA/LDEP1/LDEP1_2/codigo_quebrado.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int idade;
+
+    scanf("%d", &idade);
+
+    if (idade < 16) {
+        printf("vc nao pode votar!\n");
+    }
+    else if ( idade >= 16 && idade < 18) {
+        printf("vc pode votar, mas nao eh obrigado!\n");
+    }
+    else if (idade >= 18 && idade < 65) {
+        printf("vc eh obrigado a votar!\n");
+    }
+    else if (idade >= 65) {
+        printf("vc pode votar, mas nao eh obrigado!\n");
+    }
+
+    return 0;
+}
