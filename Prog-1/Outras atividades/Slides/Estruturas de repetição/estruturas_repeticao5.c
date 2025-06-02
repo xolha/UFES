7e0c3a49d2359d61ed4af44905e6f34df9ad8720
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-06-02 07:42:48 -0300

    ✨ feat: add exercise "estruturas_repeticao5"

---
+++ Prog-1/Outras atividades/Slides/Estruturas de repetição/estruturas_repeticao5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    double c = 1.5, z = 1.1;
+
+    do {
+        z = z + 0.3;
+        c = c + 0.2;
+        printf("ira demorar %.1f ano para que ze seja maior que chico", z = c);
+    } while ( z < c );
+
+    return 0;
+}
