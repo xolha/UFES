c2dcf9a58203ea50e2e2430a04a458eb010916b2
xolha <juliaportojpa@gmail.com>
2026-03-29 11:47:41 -0300

    🔥 feat: add m1 exercise

---
+++ Prog1/m1/m1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //primeira coisa ler dois numeros INTERIROIS a b
+    int a, b;
+
+    scanf("%d %d", &a, &b );
+
+    //segundo printar eles em ordem invertida e com virgula b,a
+    printf("%d,%d", b, a);
+    //dar o barra n no final
+
+    return 0;
+}
