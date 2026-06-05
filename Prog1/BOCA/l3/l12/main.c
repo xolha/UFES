226907e94bd4e0a74b47d20ea174a559ecf73232
xolha <juliaportojpa@gmail.com>
2026-06-05 16:43:09 -0300

    ✨ feat: add l3_12 exercise

--- Prog1/BOCA/l3/l12/main.c
+++ Prog1/BOCA/l3/l12/main.c
@@ -6,48 +6,25 @@ void valorPrimo(int n);
 
 int main(){
     int a;
-    scanf("%d", &a);
+    while (scanf("%d", &a) == 1) {
+        int soma;
+        soma = somadosdigitos(a);
 
-    parImpar(a);
-    valorPrimo(a);
+        //distrincha os numeros até ficar somente um digito
+        while(1){
+            printf("%d ", soma);
+            parImpar(soma);
+            printf(" ");
+            valorPrimo(soma);
+            printf("\n");
 
-
-    //SE A SOMA FOR NÃO PRIMO, FAZ OUTRA COISA PARA PEGAR O PRIMO
-    int verificaPar = somadosdigitos(a);
-    if (verificaPar % 2 == 0) {
-        int ehPrimo = 1, i = 0, entaoPrimo = -1;
-        
-        //faz a mesma lógica assim do primo, mas acho q eu poderia só ter pego a função embaixo
-        while (a >= 1){
-            int resto = 0;
-            ehPrimo = 1;
-
-            resto = a % 10;
-            if (resto <= 1) {
-                ehPrimo = 0;
-            } else {
-            for (i = 2; i < resto; i++){
-                if ( resto % i == 0) {
-                    ehPrimo = 0;
-                    break;
-                }
-            }
+            if (soma < 10) {
+                break;
             }
 
-            if (ehPrimo == 1){
-                entaoPrimo = resto;
-            }
-            a = a / 10;
+            soma = somadosdigitos(soma);
         }
-        printf("\n");
-
-        if (entaoPrimo != -1) {
-        parImpar(entaoPrimo);
-        printf("Primo\n");
-        }
-
     }
-
     return 0;
 }
 
@@ -56,7 +33,7 @@ int somadosdigitos(int n){
     int soma = 0;
     
     // soma todo os digitos do número
-    while (n >= 1){
+    while (n > 0){
         int resto = 0;
         //pega o ultimo valor
         resto = n % 10;
@@ -69,15 +46,11 @@ int somadosdigitos(int n){
 }
 
 void parImpar(int n){
-    int digitos = somadosdigitos(n);
-    int ehPar = 1;
 
-    if (digitos % 2 == 0) {
-        printf("%d Par ", digitos);
-        ehPar = 1;
+    if (n % 2 == 0) {
+        printf("Par");
     } else {
-        printf("%d Impar ", digitos);
-        ehPar = 0;
+        printf("Impar");
     }
 }
 
@@ -85,9 +58,11 @@ void valorPrimo(int n) {
     int i = 0, ehPrimo = 1;
     int digito = somadosdigitos(n);
 
-    
     if (n <= 1 ){
         ehPrimo = 0;
+        printf("Nao e primo");
+
+        return;
     } else {
             //varre os numeros até encontrar algum valor q é divísivel com número analisado
 
